f6f7b684d70f3b9deb58c9f1168ab29f09a871af
laiqb <laiqbudye@gmail.com>
2020-06-24 03:49:17 +0530

    Production || changing endpoint URL with llive URL

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ export const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [users, setUsers] = useState('');
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://chit-chat1.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
