1fbc336311c669cac2ad64395a0475009c405da6
laiqbudye <laiqbudye@gmail.com>
2020-06-24 01:52:00 +0530

    Input Box || Chat || added input box which will take inputs from user and send button will send the msg

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Input.css';
+
+export const Input = ({ message, setMessage, sendMessage}) => {
+    return (
+        <form className='form'>
+            <input
+                className='input'
+                type='text'
+                placeholder='Type message'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null}
+            />
+
+            <button className='sendButton'
+            onClick = {(e) => sendMessage(e)}>
+                Send
+            </button>
+        </form>
+    )
+}
